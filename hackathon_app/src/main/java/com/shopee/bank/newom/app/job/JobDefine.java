93bf42fb992f9db7943fe3115151cd9380011abb
quzekai <fsg2dev@shopee.com>
2021-12-22 11:06:55 +0800

    提交代码

---
+++ hackathon_app/src/main/java/com/shopee/bank/newom/app/job/JobDefine.java
@@ -0,0 +1,12 @@
+package com.shopee.bank.newom.app.job;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class JobDefine implements InitializingBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        //Scheduled scheduled = StdSchedulerFactory.
+    }
+}
