6897daa742b8577943aa32d96bd304f80456765d
quzekai <fsg2dev@shopee.com>
2021-12-24 10:00:07 +0800

    提交代码

--- hackathon_app/src/main/java/com/shopee/bank/newom/app/job/DealHotAcctBal.java
+++ hackathon_app/src/main/java/com/shopee/bank/newom/app/job/DealHotAcctBal.java
@@ -4,10 +4,15 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.text.SimpleDateFormat;
+
 public class DealHotAcctBal implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("更新热点账户余额");
+        Long time = System.currentTimeMillis();
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        System.out.println(simpleDateFormat.format(time)+": 更新热点账户余额");
     }
 }
--- hackathon_app/src/main/java/com/shopee/bank/newom/app/service/impl/UserServiceImpl.java
+++ hackathon_app/src/main/java/com/shopee/bank/newom/app/service/impl/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.shopee.bank.newom.app.bean.UserTab;
 import com.shopee.bank.newom.app.dao.UserTabMapper;
 import com.shopee.bank.newom.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -14,9 +17,24 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserTabMapper userTabMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public List<UserTab> findById(String userId) {
-        List<UserTab> list = userTabMapper.Select(userId);
-        return list;
+
+        ValueOperations<String,List<UserTab>> valueOperations = redisTemplate.opsForValue();
+        boolean haskey = redisTemplate.hasKey(userId);
+        if(haskey){
+            List<UserTab> list = valueOperations.get(userId);
+            System.out.println("从缓存获取数据----");
+            return list;
+        }else {
+            List<UserTab> list = userTabMapper.Select(userId);
+            System.out.println("从数据库获取数据----");
+            valueOperations.set(userId,list);
+            redisTemplate.expire(userId, Duration.ofMillis(5000));
+            return list;
+        }
     }
 }
